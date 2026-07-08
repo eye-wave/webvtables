c0554691ea9b3f5578623e3827652abdf819f171
eyewave <eyewave@noreply.codeberg.org>
2026-07-09 00:15:20 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,5 @@
 import { defineConfig } from "vite";
-import { minify } from "shader-minifier-wasm";
-
-const mode = process.env.NODE_ENV;
-const isProd = mode === "production";
-
-function glslMinifyPlugin() {
-  return {
-    name: "vite-plugin-glsl-minify",
-
-    async transform(code: string, id: string) {
-      if (!id.endsWith(".glsl")) return null;
-
-      const mini = isProd ? await minify({ code }, { format: "text" }) : code;
-
-      return {
-        code: `export default ${JSON.stringify(mini)};`,
-        map: null,
-      };
-    },
-  };
-}
+import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
   base: "/webvtables/",
@@ -34,5 +14,5 @@ export default defineConfig({
   build: {
     modulePreload: false,
   },
-  plugins: [glslMinifyPlugin()],
+  plugins: [glsl({ minify: true })],
 });
